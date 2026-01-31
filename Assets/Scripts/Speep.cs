b32f5f8d391cd7fa38f87c4bb6afc89b34f01354
SamHemming <32447304+SamHemming@users.noreply.github.com>
2026-01-31 13:36:10 +0200

    Speeps and prefabs

--- Assets/Scripts/Speep.cs
+++ Assets/Scripts/Speep.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 public class Speep : MonoBehaviour
 {
-	public Sprite maskedSprite;
 	public bool isMasked = false;
 
-	private SpriteRenderer sr;
+	private Animator animator;
 
 	private void Start()
 	{
-		sr = GetComponent<SpriteRenderer>();	
+		animator = GetComponent<Animator>();	
 	}
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
-		if (!isMasked) sr.sprite = maskedSprite;
-		isMasked = true;
-		StageManager.Singleton.CheckMasks();
+		if (!isMasked)
+		{
+			isMasked = true;
+			animator.SetBool("isMasked", true);
+			StageManager.Singleton.CheckMasks();
+		}
 	}
 }
